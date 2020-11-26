9fc22148921cdc3f338bfef6f16bca3ba9823a32
zchong <zhch_hong@163.com>
2020-11-26 10:39:15 +0800

    ncu -u

--- src/views/file-manage/index.vue
+++ src/views/file-manage/index.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>文件管理</h1>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { readdirSync } from 'fs';
+
+const dirPath = 'D:\\JyQipai_doc';
+
+@Component
+export default class FileManage extends Vue {
+  created(): void {
+    const dir = readdirSync(dirPath);
+    console.log(dir);
+  }
+}
+</script>
