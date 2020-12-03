4592ec3b29b96f142a0631f652efd8873543c366
zchong <zhch_hong@163.com>
2020-12-04 01:08:47 +0800

    [fix]时间戳转换

--- src/views/edit-task/writeExcel.ts
+++ src/views/edit-task/writeExcel.ts
@@ -1,6 +1,6 @@
 import store from '@/store';
 import { writeFileSync } from 'fs';
-import { Workbook } from 'exceljs';
+import { CellValue, Workbook, Worksheet } from 'exceljs';
 
 import { sheetToJson } from '@/utils/sheetToJson';
 import { writeFile } from '@/utils/fileStream';
@@ -21,8 +21,50 @@ export function writeExcel(data: Record<string, any>) {
 
 function writeBase(data: Record<string, any>) {
   const ws = workbook.getWorksheet('task');
+  const rowData = getRowByColumnValue(ws, 'id', data.id);
+  const row = rowData.rowData as Record<string, CellValue>;
+  console.log(JSON.parse(JSON.stringify(row)));
+  const rowNumber = rowData.rowNumber as number;
+  row['任务内容说明'] = data.desc;
+  delete data.desc;
+  Object.assign(row, data);
 
-  // workbook.xlsx.writeBuffer().then((buffer) => {
-  //   writeFileSync(path, new Uint8Array(buffer));
-  // });
+  if (
+    typeof row.start_valid_time === 'string' &&
+    typeof row.end_valid_time === 'string'
+  ) {
+    row.start_valid_time = parseInt(row.start_valid_time) * 1000;
+    row.end_valid_time = parseInt(row.end_valid_time) * 1000;
+  }
+
+  console.log(JSON.parse(JSON.stringify(row)));
+  ws.spliceRows(rowNumber, 1);
+  ws.insertRow(rowNumber, row);
+  workbook.xlsx.writeBuffer().then((buffer) => {
+    writeFileSync(path, new Uint8Array(buffer));
+  });
+}
+
+function getRowByColumnValue(
+  ws: Worksheet,
+  col: string,
+  value: string
+): Record<string, Record<string, CellValue> | number> {
+  const object: Record<string, Record<string, CellValue> | number> = {};
+  const rowData: Record<string, CellValue> = {};
+  ws.eachRow((row, index) => {
+    const cell = row.getCell(col);
+    if (cell.toString() === value) {
+      object['rowNumber'] = index;
+      const row = ws.getRow(index);
+      row.eachCell((cell, colNumber) => {
+        const key = cell.worksheet.getColumn(colNumber).key;
+        if (key) {
+          rowData[key] = cell.value;
+        }
+      });
+    }
+  });
+  object['rowData'] = rowData;
+  return object;
 }
