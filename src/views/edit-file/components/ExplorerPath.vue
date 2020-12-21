5a729058d18b59ff8abb1b2988cc5909a6b91e1c
zchong <zhch_hong@163.com>
2020-12-21 23:21:40 +0800

    feat: 在资源管理器和默认程序中打开文件

--- src/views/edit-file/components/ExplorerPath.vue
+++ src/views/edit-file/components/ExplorerPath.vue
@@ -1,11 +1,35 @@
 <template>
-  <span>{{ path }}</span>
+  <span class="explorer-path" :title="path" @click="linkExplorer($event)">{{
+    explorerpath
+  }}</span>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { shell } from 'electron';
+
+import { userConfig } from '@/asserts/userconfig';
 
 @Component
 export default class ExplorerPath extends Vue {
   @Prop({ type: String, default: '' }) path!: string;
+
+  get explorerpath(): string {
+    return this.path.slice(userConfig.workDir.length + 1);
+  }
+
+  linkExplorer(event: MouseEvent): void {
+    if (event.ctrlKey) shell.openExternal(this.path);
+    else shell.showItemInFolder(this.path);
+  }
 }
 </script>
+<style lang="scss" scoped>
+.explorer-path {
+  margin-left: 20px;
+  cursor: pointer;
+  color: #1b9de3;
+  &:hover {
+    text-decoration-line: underline;
+  }
+}
+</style>
