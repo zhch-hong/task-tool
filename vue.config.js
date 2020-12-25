6a3fcd297b4011106fb465c6db6bab39f9342727
zchong <zhch_hong@163.com>
2020-12-25 15:34:10 +0800

    fix: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,7 @@ module.exports = {
       nodeIntegration: true,
       builderOptions: {
         win: {
-          target: ['nsis'],
           icon: 'src/assets/icon_256.ico',
-        },
-        nsis: {
-          installerIcon: 'src/assets/icon_256.ico',
         },
       },
     },
