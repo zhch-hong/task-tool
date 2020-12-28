c919daca70fb086828680609bd993dead2400fcd
zchong <zhch_hong@163.com>
2020-12-28 10:50:55 +0800

    refactor: 读取最后一次打开的文件

--- src/asserts/lastOpenFile.ts
+++ src/asserts/lastOpenFile.ts
@@ -1,26 +1,41 @@
 import store from '@/store';
+import { statSync } from 'fs';
 import { setColumnKey, workbook2map } from '@/utils';
 import { Workbook } from 'exceljs';
 import { getUserconfig } from '@/asserts/userconfig';
 import { readFileBinary } from '@/utils/fileSystem';
 
-export async function readLastFile(): Promise<string | undefined> {
-  // 从配置文件读取最后一次打开的文件
-  const object: Record<string, string> = getUserconfig();
-  const { lastOpenFile } = object;
+export function readLastFile() {
+  return new Promise<string>((resolve, reject) => {
+    // 从配置文件读取最后一次打开的文件
+    const object: Record<string, string> = getUserconfig();
+    const { lastOpenFile } = object;
 
-  if (!lastOpenFile) return;
+    if (!lastOpenFile) {
+      reject();
+      return;
+    }
 
-  const buffer = readFileBinary(lastOpenFile);
-  if (!buffer) return;
+    const stat = statSync(lastOpenFile);
+    if (stat.isDirectory()) {
+      reject();
+      return;
+    }
 
-  const workbook = new Workbook();
-  await workbook.xlsx.load(buffer);
+    const buffer = readFileBinary(lastOpenFile);
+    if (!buffer) {
+      reject();
+      return;
+    }
 
-  setColumnKey(workbook);
+    const workbook = new Workbook();
+    workbook.xlsx.load(buffer).then(() => {
+      setColumnKey(workbook);
 
-  store.commit('taskFilePath', lastOpenFile);
-  store.commit('workbookMap', workbook2map(workbook));
+      store.commit('taskFilePath', lastOpenFile);
+      store.commit('workbookMap', workbook2map(workbook));
 
-  return lastOpenFile;
+      resolve(lastOpenFile);
+    });
+  });
 }
