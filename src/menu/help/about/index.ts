e76855cb3c16a105c4973e947630852d23f449d1
zchong <zhch_hong@163.com>
2021-03-12 17:28:11 +0800

    feat: about

--- src/menu/help/about/index.ts
+++ src/menu/help/about/index.ts
@@ -1,13 +1,38 @@
-import { remote } from 'electron';
+import Vue from 'vue';
+import { Dialog } from 'element-ui';
+import About from './About.vue';
 
-const { app, dialog, getCurrentWindow } = remote;
+const obser = Vue.observable({ visible: false });
+const ComponentClass = Vue.extend({
+  render(h) {
+    return h(
+      Dialog,
+      {
+        props: {
+          title: '任务配置工具',
+          visible: obser.visible,
+          modal: false,
+          width: '30%',
+          'close-on-click-modal': false,
+          'custom-class': 'el-dialog__custom-class',
+        },
+
+        on: {
+          'update:visible': (visible: boolean) => {
+            obser.visible = visible;
+          },
+        },
+      },
+      [h(About)]
+    );
+  },
+});
+
+const instance = new ComponentClass();
+const div = document.createElement('div');
+document.body.append(div);
+instance.$mount(div);
 
 export function about() {
-  const win = getCurrentWindow();
-  win.focus();
-  dialog.showMessageBoxSync(win, {
-    title: '关于',
-    message: '任务配置工具',
-    detail: `版本：${app.getVersion()}`,
-  });
+  obser.visible = true;
 }
